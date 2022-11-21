6d68643a97a039eef5e9489840a58a14ee360437
PARKCHAEYEONGG <118724460+PARKCHAEYEONGG@users.noreply.github.com>
2022-11-21 19:43:56 +0900

    Add files via upload

---
+++ Action.java
@@ -0,0 +1,11 @@
+package shop.admin.goods.action;
+
+import javax.servlet.http.*;
+
+public interface Action {
+	
+	public ActionForward execute(HttpServletRequest request, HttpServletResponse response)throws Exception;
+	
+	
+
+}
